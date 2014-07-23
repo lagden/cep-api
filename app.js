401246c29ae3a1a718da04b90d8080be819349b5
Thiago Lagden <lagden@gmail.com>
2014-07-23 14:04:37 -0300

    fix cors

--- app.js
+++ app.js
@@ -58,10 +58,10 @@ app.use(function(err, req, res, next) {
 });
 
 // CORS
-app.all('*', function(req, res, next) {
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET');
+    // res.header('Access-Control-Allow-Methods', 'GET');
     next();
 });
 
