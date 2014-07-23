9f61c97b66c4d2f516945c5e83e249b194d5ba49
Thiago Lagden <lagden@gmail.com>
2014-07-23 14:26:44 -0300

    fix name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'CEP Api' });
+  res.render('index', { title: 'CEP API v1' });
 });
 
 module.exports = router;
